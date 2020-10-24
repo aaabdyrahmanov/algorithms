57394df5a330098d6022b04d0baf438d4251f9bd
atamyratabdy <aaabdyrahmanov@gmail.com>
2020-10-24 23:56:44 +0300

    Add solution of the "Numbers in strings" challenge

---
+++ Numbers in strings/index.js
@@ -0,0 +1,13 @@
+function solve(s){
+    return Math.max(...s.match(/\d+|$/g))
+  };
+
+    /**
+   * 
+   * solve('gh12cdy695m1'),695
+   * 
+   * solve('vih61w8oohj5'),61
+   * 
+   * solve('lu1j8qbbb85'),85
+   * 
+   */
