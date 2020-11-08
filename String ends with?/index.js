77754fb1e834981d85f0776585c921342419878e
atamyratabdy <aaabdyrahmanov@gmail.com>
2020-11-08 23:16:58 +0300

    Add solution of the "String ends with?" challenge

---
+++ String ends with?/index.js
@@ -0,0 +1,13 @@
+function solution(str, ending){
+    return str.endsWith(ending)
+  }
+  
+    /**
+   * 
+   * solution('abcde', 'cde'), true
+   * 
+   * solution('samurai, ra), false
+   * 
+   * soltion('!@#$%^&*() :-)', ':-)'), true
+   * 
+   */
