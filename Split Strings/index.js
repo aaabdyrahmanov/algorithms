51eb467f762d9e7abf15ec9a1298d680d5df9fb8
Atamyrat Abdyrahmanov <aaabdyrahmanov@gmail.com>
2020-12-14 22:57:35 +0300

    Add solution of the "Split Strings" challenge

---
+++ Split Strings/index.js
@@ -0,0 +1,18 @@
+function solution(str){
+    const arr = []
+    for(let i=0; i<str.length;i=i+2) {
+      arr.push(str[i] + str[i+1])
+    }
+   arr[arr.length-1] ? arr[arr.length-1]=arr[arr.length-1].replace("undefined", "_") : null
+   return arr
+ }
+
+    /**
+   * 
+   * solution(""), []
+   * 
+   * solution("abcdef"), ["ab", "cd", "ef"]
+   * 
+   * solution("abcdefg"), ["ab", "cd", "ef", "g_"]
+   * 
+   */
