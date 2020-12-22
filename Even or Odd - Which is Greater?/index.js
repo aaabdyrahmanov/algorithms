6400f0bf2950d1f76de01aef4237389dd2cddedf
Atamyrat Abdyrahmanov <aaabdyrahmanov@gmail.com>
2020-12-22 20:56:12 +0300

    Add solution of the "Even or Odd - Which is Greater?" challenge

---
+++ Even or Odd - Which is Greater?/index.js
@@ -0,0 +1,23 @@
+function evenOrOdd(str) {
+    let odds = 0,
+        evens = 0;
+    for(let i=0; i<str.length; i++) {
+      if(Number(str[i]) % 2) {
+        odds += Number(str[i])
+      } else {
+        evens += Number(str[i])
+      }
+    }
+    
+    return odds>evens ? 'Odd is greater than Even' : (odds<evens ? 'Even is greater than Odd' : 'Even and Odd are the same')
+  }
+
+    /**
+   * 
+   * evenOrOdd('12'), 'Even is greater than Odd'
+   *     
+   * evenOrOdd('121'), 'Even and Odd are the same'
+   * 
+   * evenOrOdd('12345'), 'Odd is greater than Even'
+   * 
+   */
