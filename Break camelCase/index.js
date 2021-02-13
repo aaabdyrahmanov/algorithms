522ca6777f1a86ce2af1d3e8c9dc00e17f01d40e
Atamyrat Abdyrahmanov <aaabdyrahmanov@gmail.com>
2021-02-13 11:57:41 +0300

    Add solution of the "Break camelCase" challenge

---
+++ Break camelCase/index.js
@@ -0,0 +1,20 @@
+function solution(string) {
+    let arr = []
+    
+    for(let i=0; i<string.length; i++) {
+      if(string[i] == string[i].toUpperCase()) arr.push(i)
+    }
+    arr.map((m,i)=> string = [string.slice(0, m+i), ' ', string.slice(m+i)].join(''))  
+    
+    return string;
+  }
+
+    /**
+   * 
+   * solution('camelCasing') => 'camel Casing'
+   * 
+   * solution('camelCasingTest') => 'camel Casing Test'
+   * 
+   * solution('camelCaseLetters'), 'camel Case Letters'
+   * 
+   */
