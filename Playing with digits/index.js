09010434547d591f70fa4b4db8fccdee7b79388c
Atamyrat Abdyrahmanov <aaabdyrahmanov@gmail.com>
2021-02-21 05:15:34 +0300

    Add solution of the "Playing with digits" challenge

---
+++ Playing with digits/index.js
@@ -0,0 +1,19 @@
+function digPow(n, p){
+    let x = String(n), 
+        t = 0
+    for(let i=p-1; i<x.length+p-1; i++) {
+      t += Math.pow(Number(x[i-p+1]), i+1) 
+    }
+  
+    return t%n == 0 ? t/n : -1
+  }
+
+    /**
+   * 
+   * digPow(89, 1) => 1
+   * 
+   * digPow(92, 1) => -1
+   * 
+   * digPow(46288, 3) => 51
+   * 
+   */
