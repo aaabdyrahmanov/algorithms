71f57f93993092918943d3f085425bbeb75336a4
Atamyrat Abdyrahmanov <atamyrat@abdyrahmanov.com>
2023-01-07 13:00:06 +0100

    ♻️ Refactor the "Add Digits" solution

--- 258. Add Digits/index.js
+++ 258. Add Digits/index.js
@@ -1,16 +1,7 @@
 module.exports = function addDigits(number) {
-  let answer = number;
-
-  while (answer > 9) {
-    let sum = 0;
-
-    while (answer > 0) {
-      const current = answer % 10;
-      sum += current;
-      answer = (answer - current) / 10;
-    }
-    answer = sum;
+  while (number > 9) {
+    number = number.toString().split('').reduce((acc, curr) => acc + Number(curr), 0);
   }
 
-  return answer;
+  return number;
 };
